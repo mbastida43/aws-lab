a4099cfbbf098f4808e12a9d7cd275084cf40cf2
Fabricio Eiras <65662267+fabricio-eiras-ativy@users.noreply.github.com>
2021-05-22 00:03:36 -0300

    create s3

---
+++ s3.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+### Create folder client name in AWS S3 Bucket ###
+aws s3api put-object --bucket tf-NAME --key NAME-OF-DIR/
+sleep 2
+
+### Upload to AWS S3 ###
+aws s3 cp create-ec2.tf s3://tf-NAME/NAME-OF-DIR/
+sleep 2
