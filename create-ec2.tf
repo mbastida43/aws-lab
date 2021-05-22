c5609b58de8d782017668b4d020972a09054729b
Fabricio Eiras <65662267+fabricio-eiras-ativy@users.noreply.github.com>
2021-05-21 23:51:27 -0300

    create simple instance

---
+++ create-ec2.tf
@@ -0,0 +1,58 @@
+terraform {
+  required_providers {
+    aws = {
+      source = "hashicorp/aws"
+      version = "3.42.0"
+    }
+  }
+}
+
+provider "aws" {
+  region = "sa-east-1"
+  shared_credentials_file = "/var/lib/jenkins/.aws/credentials"
+  profile = "default"
+}
+
+resource "aws_instance" "ec2-instance" {
+  ami                         = "YOUR-AMI"
+  key_name                    = "YOUR-KEY"
+  subnet_id                   = "YOUR-SUBNET"
+  instance_type               = "YOUR-INSTANCE-TYPE"
+  associate_public_ip_address = "true" ### TRUE Elastic IP -- Public IP
+
+  tags = {
+      Name  = "NAME-INSTANCE" ### Appear in web console ec2 dashboard
+    }
+
+  root_block_device {
+    delete_on_termination = true
+  }
+
+  vpc_security_group_ids = ["YOUR-SEC-GROUP"]
+}
+
+resource "aws_eip_association" "eip_assoc" {
+  instance_id   = aws_instance.ec2-instance.id
+  allocation_id = aws_eip.eip_assoc.id
+}
+
+
+resource "aws_eip" "eip_assoc" {
+  vpc = true
+
+  tags = {
+      Name  = "NAME-EIP" ### Appear in web console Elastic IP dashboard 
+    }
+
+   depends_on = [aws_instance.ec2-instance]
+}
+
+terraform {
+  backend "consul" {
+    address  = "IP-CONSUL:8500"
+    scheme   = "http"
+    path     = "tf/NAME-terraform.tfstate"
+    lock     = true
+    gzip     = false
+  }
+}
