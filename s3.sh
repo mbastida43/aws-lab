113c3c514dfea73a366950a4c39ae9251ec4b01c
Fabricio Eiras <65662267+fabricio-eiras-ativy@users.noreply.github.com>
2021-05-24 09:18:48 -0300

    Update s3.sh

--- s3.sh
+++ s3.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-### Create folder client name in AWS S3 Bucket ###
+### Create folder client name in AWS S3 Bucket, this script run on local of .tf ###
 aws s3api put-object --bucket tf-NAME --key NAME-OF-DIR/
 sleep 2
 
