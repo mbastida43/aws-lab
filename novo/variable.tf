44904370386931156c3b17827a3b693e9d5bb1f0
Fabricio Eiras <65662267+fabricio-eiras-ativy@users.noreply.github.com>
2021-06-15 16:39:24 -0300

    Create variable.tf

---
+++ novo/variable.tf
@@ -0,0 +1,35 @@
+variable "my2k19key" {
+  type    = string
+  default = "win-2019-my"
+}
+
+variable "mysubnet" {
+  type    = string
+  default = "subnet-xxxxxxxxxx"
+}
+
+variable "myec2type" {
+  type    = string
+  default = "t2.large"
+}
+
+variable "mysg" {
+  type = map(list(string))
+  default = {
+    "sgc-my-default" = ["sg-xxxxxxxxxxxx"]
+  }
+}
+
+variable "default_tags" {
+  type = map(string)
+  default = {
+    Billing        = "XXXXX"
+    Terraform      = "yes"
+    Backup         = "yes"
+  }
+}
+
+variable "cloudflare_zone_id" {
+  type    = string
+  default = "XXXXXXXXXXXXXXXXXXXXXXX"
+}
